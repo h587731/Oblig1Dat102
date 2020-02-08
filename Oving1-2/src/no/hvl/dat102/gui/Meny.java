2e4a243b5f605e64464d154ed51fffcb62a6d88a
Windhoes\Eskil <mail.eskil@gmail.com>
2020-02-08 12:44:56 +0100

    3 test branch test

--- Oving1-2/src/no/hvl/dat102/gui/Meny.java
+++ Oving1-2/src/no/hvl/dat102/gui/Meny.java
@@ -73,6 +73,8 @@ public class Meny {
 		// lager meny elementer, og deres under elementer og fester dem med metoder og
 		// action listener med metoden attachComponents
 		MenyComponent.attachComponents(fil, this, "Nytt", "Opprett", 1);
+		System.out.println("YOL sss");
+
 //		MenyComponent.attachComponents(fil, this, "Åpne", "Åpne", 2);
 //		MenyComponent.attachComponents(fil, this, "Lagre", "Lagre", 3);
 //		MenyComponent.attachComponents(fil, this, "Lagre som", "Lagre som", 4);
